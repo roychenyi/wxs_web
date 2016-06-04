211fa7cc56a4b00c821720493544df2cad3e46d2
roychenyi <852416288@qq.com>
2016-06-04 22:21:22 +0800

    friend

--- js/user/friend.js
+++ js/user/friend.js
@@ -1,8 +1,49 @@
 /**
  * Created by chenyi on 2016/06/04.
  */
+    $(document).ready(function(){
+        friend.show();
+    });
 var friend={
-    add:function(){
+    //显示添加好友的状态
+    show:function(){
+
+    },
+    //添加好友
+    add:function(url){
+        ajax1(url,'',friend.add_reslut);
+    },
+    add_result:function(data){
+
+    },
+    //删除好友
+    delete:function(url){
+        ajax1(url,'',friend.delete_result);
+    },
+    delete_result:function(data){
+
+    },
+    //获取好友列表
+    list:function(url){
+        ajax(url,'',friend.list_result);
+    },
+    list_result:function(data){
+
+    },
+    //获取好友分组
+    group:function(url){
+        ajax(url,'',friend.group_result);
+    },
+    group_result: function (data) {
+
+    },
+    changeGroup:function(url){
+        ajax(url,'',friend.changeGroup_result);
+    },
+    changeGroup_result:function(data){
 
     }
+
+
+
 }
